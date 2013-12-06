85e2b7bbeecbae82e17051c1095642988a26de43
Manav Rathi <manav.r@directi.com>
2013-12-06 11:49:41 +0530

    Revert to lowercase macro arguments
    They match the convention in `TDTAssert.h`

--- TDTHotChocolate/FoundationAdditions/TDTSuppressPerformSelectorLeakWarning.h
+++ TDTHotChocolate/FoundationAdditions/TDTSuppressPerformSelectorLeakWarning.h
@@ -1,10 +1,10 @@
 // http://stackoverflow.com/a/7933931/141220
 
-#define TDTSuppressPerformSelectorLeakWarning(CODE) \
+#define TDTSuppressPerformSelectorLeakWarning(code) \
   do { \
     _Pragma("clang diagnostic push") \
     _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-    CODE; \
+    code; \
     _Pragma("clang diagnostic pop") \
   } while (0)
 
