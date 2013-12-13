e238ed9aab5b258f44506eff084eaf8672f586cb
Manav Rathi <manav@talk.to>
2013-12-13 18:14:08 +0530

    Add header textual sanity checks to test suite

---
+++ Tests/Tests/TDTUmbrellaHeaderTextualSanityTests.m
@@ -0,0 +1,6 @@
+
+// Just include all umbrella headers to confirm that there are no typos.
+#import <TDTChocolate/TDTFoundationAdditions.h>
+#import <TDTChocolate/TDTTestingAdditions.h>
+#import <TDTChocolate/TDTCoreDataAdditions.h>
+#import <TDTChocolate/TDTSecurityAdditions.h>
