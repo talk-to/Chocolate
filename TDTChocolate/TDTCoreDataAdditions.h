df849cb8a835ccd766f8dc476a692e2b13b15a4f
Manav Rathi <manav@talk.to>
2013-12-13 18:11:18 +0530

    Conditionally import NFRC additions only on iOS

--- TDTChocolate/TDTCoreDataAdditions.h
+++ TDTChocolate/TDTCoreDataAdditions.h
@@ -1,4 +1,7 @@
 #import "CoreDataAdditions/NSManagedObject+TDTAdditions.h"
 #import "CoreDataAdditions/NSManagedObjectContext+TDTAdditions.h"
-#import "CoreDataAdditions/NSFetchedResultsController+TDTAdditions.h"
 #import "CoreDataAdditions/TDTCoreDataUtilityMacros.h"
+
+#ifdef TARGET_IPHONE_OS
+  #import "CoreDataAdditions/NSFetchedResultsController+TDTAdditions.h"
+#endif
