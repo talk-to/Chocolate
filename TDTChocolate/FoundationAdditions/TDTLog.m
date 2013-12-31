587c423e460e6711eb3c9a405784fb4d57ddefd2
Manav Rathi <manav@talk.to>
2013-12-31 12:59:18 +0530

    Migrate TDTLog implementation comments from iTalkTo

--- TDTChocolate/FoundationAdditions/TDTLog.m
+++ TDTChocolate/FoundationAdditions/TDTLog.m
@@ -3,6 +3,20 @@
 
 TDTLogErrorWarningHookFunction TDTLogErrorWarningHook;
 
+// When TDTLog is invoked, it prints the current date in ISO 8601
+// format first thing on the line, followed by a single space, followed
+// by the formatted string provided by the user, followed by newline.
+//
+// TDTLog uses a static date formatter to print the date string. Since it
+// doesn't rely on creating a fresh instance of NSCalendar everytime (I
+// don't even know why NSLog needs it), its performance is much better
+// than that of NSLog. e.g. Here's how much time it takes to print "Hello
+// World" a 1000 times on an iPhone 4S:
+//
+// NSLog: ~1.7s
+// TDTLog: ~0.1s
+// Plain fprintf (without any date string): ~0.01s
+
 void __TDTLog(BOOL shouldInvokeErrorWarningHook, NSString *format, ...) {
   va_list args;
   va_start(args, format);
