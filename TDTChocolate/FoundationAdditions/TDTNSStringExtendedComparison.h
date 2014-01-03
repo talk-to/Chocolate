b0fc8ac8460637036a1898f08d51272141fc8953
Manav Rathi <manav@talk.to>
2014-01-03 17:51:33 +0530

    Remove trailing whitespace

--- TDTChocolate/FoundationAdditions/TDTNSStringExtendedComparison.h
+++ TDTChocolate/FoundationAdditions/TDTNSStringExtendedComparison.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
 /**
- Perform an extended equality check between two strings by considering `nil`, 
+ Perform an extended equality check between two strings by considering `nil`,
  empty strings and strings with only whitespace and newline characters to
  be equal (on top of the usual string equality).
  */
