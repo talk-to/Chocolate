30874adc2d711bb1c351e46b70a1c82bfa61338a
Manav Rathi <manav@talk.to>
2014-01-21 15:29:29 +0530

    Document Time Interval Infinity

--- TDTChocolate/TestingAdditions/TDTPrimitiveConstants.h
+++ TDTChocolate/TestingAdditions/TDTPrimitiveConstants.h
@@ -1,3 +1,8 @@
 #import <Foundation/Foundation.h>
 
+/**
+ A very large time interval, suitable for being used in tests that do want
+ to delay the execution of certain code (e.g. a dispatch_block_t) for so long
+ that it can be effectively ignored.
+ */
 extern const NSTimeInterval TDTNSTimeIntervalInfinity;
