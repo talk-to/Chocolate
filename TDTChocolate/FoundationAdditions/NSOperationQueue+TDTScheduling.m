c6679afe206a6ba2a5d28a37607fe2cdc262f4ec
Manav Rathi <manav@talk.to>
2014-02-07 13:46:22 +0530

    Add asserts to tdt_addOperation:afterDelay:

--- TDTChocolate/FoundationAdditions/NSOperationQueue+TDTScheduling.m
+++ TDTChocolate/FoundationAdditions/NSOperationQueue+TDTScheduling.m
@@ -1,9 +1,13 @@
 #import "NSOperationQueue+TDTScheduling.h"
+#import "TDTAssert.h"
 
 @implementation NSOperationQueue (TDTScheduling)
 
 - (void)tdt_addOperation:(NSOperation *)operation
               afterDelay:(NSTimeInterval)delay {
+  TDTParameterAssert(operation);
+  TDTAssert(delay > 0);
+
   __weak typeof(self) weakSelf = self;
   double delayInSeconds = delay;
   dispatch_queue_t arbitraryQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
