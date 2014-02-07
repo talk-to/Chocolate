88db9ea2bd5e4861c8a79c8db73e2497bd6a0e05
Manav Rathi <manav@talk.to>
2014-02-07 13:44:49 +0530

    Document tdt_addOperation:afterDelay:

--- TDTChocolate/FoundationAdditions/NSOperationQueue+TDTScheduling.h
+++ TDTChocolate/FoundationAdditions/NSOperationQueue+TDTScheduling.h
@@ -2,6 +2,14 @@
 
 @interface NSOperationQueue (TDTScheduling)
 
+/**
+ Wait for @p delay and then add @p operation to the receiver.
+
+ @param operation The operation to add to the receiver. It will be strongly
+        referenced for @p delay` even if it cancelled or if the receiver
+        is deallocated.
+ @param delay The time interval to wait before adding @p operation.
+ */
 - (void)tdt_addOperation:(NSOperation *)operation
               afterDelay:(NSTimeInterval)delay;
 
