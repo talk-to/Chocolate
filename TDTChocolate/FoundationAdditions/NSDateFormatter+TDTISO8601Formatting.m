10a82538b00c56ff51e543389d6561e41f2c3275
Manav Rathi <manav@talk.to>
2014-02-07 09:35:02 +0530

    Replace string #define with a constant

--- TDTChocolate/FoundationAdditions/NSDateFormatter+TDTISO8601Formatting.m
+++ TDTChocolate/FoundationAdditions/NSDateFormatter+TDTISO8601Formatting.m
@@ -1,12 +1,12 @@
 #import "NSDateFormatter+TDTISO8601Formatting.h"
 
-#define ISO_DATE_FORMAT @"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
+static NSString *const ISO8601DateFormat = @"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'";
 
 @implementation NSDateFormatter (TDTISO8601Formatting)
 
 + (instancetype)ISO8601Formatter {
   NSDateFormatter *formatter = [[self alloc] init];
-  formatter.dateFormat = ISO_DATE_FORMAT;
+  formatter.dateFormat = ISO8601DateFormat;
   formatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
   formatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
   return formatter;
