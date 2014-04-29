a32f3b03d56c02033bdebf8a5f7207738f1492ab
Ayush Goel <ayush.g@directi.com>
2014-04-29 15:38:39 +0530

    Remove now irrelevant motivation section

--- TDTChocolate/FoundationAdditions/NSDictionary+TDTNullNormalization.h
+++ TDTChocolate/FoundationAdditions/NSDictionary+TDTNullNormalization.h
@@ -1,13 +1,5 @@
 #import <Foundation/Foundation.h>
 
-// Motivation:
-// JSON responses frequently contain 'null'. For our purposes, a JSON object with
-// a null property is equivalent to the same object with that property missing.
-// However, JSON parsers treat the former as `NSNull` and the latter as nil;
-// This leads to problems for the subsequent code because NSNull is a bona-fide
-// object while messages to nil are discarded. Furthermore, Objective C truthy
-// checks work for NSNull and fail for nil.
-
 @interface NSDictionary (TDTNullNormalization)
 
 /**
