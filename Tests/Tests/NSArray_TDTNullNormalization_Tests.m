2f87e50fdf9c6850e6159bb34b956c43ee5b5549
Ayush Goel <ayush.g@directi.com>
2014-04-29 13:09:05 +0530

    Add test for removing single null

--- Tests/Tests/NSArray_TDTNullNormalization_Tests.m
+++ Tests/Tests/NSArray_TDTNullNormalization_Tests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <TDTChocolate/TDTFoundationAdditions.h>
+#import <TDTChocolate/TDTTestingAdditions.h>
 
 @interface NSArray_TDTNullNormalization_Tests : XCTestCase
 
@@ -14,21 +16,10 @@
 
 @implementation NSArray_TDTNullNormalization_Tests
 
-- (void)setUp
-{
-  [super setUp];
-  // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testSingleNullIsRemoved {
+  NSArray *array = @[[NSString tdt_randomString]];
+  NSArray *arrayWithNull = [array arrayByAddingObject:[NSNull null]];
+  NSArray *normalizedArray = [arrayWithNull tdt_arrayByRemovingNulls];
+  XCTAssertEqualObjects(normalizedArray, array);
 }
-
-- (void)tearDown
-{
-  // Put teardown code here. This method is called after the invocation of each test method in the class.
-  [super tearDown];
-}
-
-- (void)testExample
-{
-  XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
 @end
