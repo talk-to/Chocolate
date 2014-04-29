f1e18c6c9c9d5fe710914354c1e8930108e89484
Ayush Goel <ayush.g@directi.com>
2014-04-29 13:23:15 +0530

    Add tests for normalization on NSDictionary

--- Tests/Tests/NSDictionary_TDTNullNormalization_Tests.m
+++ Tests/Tests/NSDictionary_TDTNullNormalization_Tests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <TDTChocolate/TDTFoundationAdditions.h>
+#import <TDTChocolate/TDTTestingAdditions.h>
 
 @interface NSDictionary_TDTNullNormalization_Tests : XCTestCase
 
@@ -14,5 +16,29 @@
 
 @implementation NSDictionary_TDTNullNormalization_Tests
 
+- (void)testItDoesNotChangeDictionaryWithNoNulls {
+  NSDictionary *dictionary = @{[NSString tdt_randomString] : [NSString tdt_randomString],
+                               [NSString tdt_randomString] : [NSString tdt_randomString]};
+  NSDictionary *normalizedDictionary = [dictionary tdt_dictionaryByRemovingNulls];
+  XCTAssertEqualObjects(dictionary, normalizedDictionary);
+}
+
+- (void)testSingleNullIsRemoved {
+  NSDictionary *dictionary = @{[NSString tdt_randomString] : [NSString tdt_randomString]};
+  NSMutableDictionary *dictionaryWithNull = [NSMutableDictionary dictionaryWithDictionary:dictionary];
+  dictionaryWithNull[[NSString tdt_randomString]] = [NSNull null];
+  NSDictionary *normalizedDictionary = [dictionaryWithNull tdt_dictionaryByRemovingNulls];
+  XCTAssertEqualObjects(dictionary, normalizedDictionary);
+}
+
+- (void)testMultipleNullsAreRemoved {
+  NSDictionary *dictionary = @{[NSString tdt_randomString] : [NSString tdt_randomString],
+                               [NSString tdt_randomString] : [NSString tdt_randomString]};
+  NSMutableDictionary *dictionaryWithNulls = [NSMutableDictionary dictionaryWithDictionary:dictionary];
+  dictionaryWithNulls[[NSString tdt_randomString]] = [NSNull null];
+  dictionaryWithNulls[[NSString tdt_randomString]] = [NSNull null];
+  NSDictionary *normalizedDictionary = [dictionaryWithNulls tdt_dictionaryByRemovingNulls];
+  XCTAssertEqualObjects(dictionary, normalizedDictionary);
+}
 
 @end
