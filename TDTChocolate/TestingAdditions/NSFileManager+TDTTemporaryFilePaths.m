305ec269565328f094aee7825a36e97ecb179c7f
Manav Rathi <manav@talk.to>
2014-09-05 12:48:08 +0530

    Remove comments from implementation files

--- TDTChocolate/TestingAdditions/NSFileManager+TDTTemporaryFilePaths.m
+++ TDTChocolate/TestingAdditions/NSFileManager+TDTTemporaryFilePaths.m
@@ -2,18 +2,11 @@
 
 @implementation NSFileManager (TDTTemporaryFilePaths)
 
-/**
- Simplified version of `tdt_fileURLToTemporaryFileWithNamePrefix:suffix:`
- */
 - (NSString *)tdt_temporaryFilePath {
   NSString *UUID = [[NSUUID UUID] UUIDString];
   return [NSTemporaryDirectory() stringByAppendingString:UUID];
 }
 
-/**
- Execute @p block with a valid temporary file path. A file written to this
- path will be automatically removed after @p block finishes execution.
- */
 - (void)tdt_withTemporaryPath:(void (^)(NSString *path))block {
   NSString *path = [self tdt_temporaryFilePath];
   block(path);
