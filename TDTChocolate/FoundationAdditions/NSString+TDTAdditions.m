4ee93ee8e4d0b2ea0ad1ba3972eef7acbc322182
Prateek <prateek.kh@directi.com>
2015-06-25 17:11:57 +0530

    Fix regex for email validation

--- TDTChocolate/FoundationAdditions/NSString+TDTAdditions.m
+++ TDTChocolate/FoundationAdditions/NSString+TDTAdditions.m
@@ -78,13 +78,7 @@
 }
 
 - (BOOL)tdt_isEmail {
-  NSString *emailRegex = @"(?:[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}"
-  @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
-  @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-zA-Z0-9](?:[a-"
-  @"z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\\[(?:(?:25[0-5"
-  @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
-  @"9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
-  @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+  NSString *emailRegex = @"[^@ ]+@[^@ ]+\\.[^@ ]+";
   NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
   BOOL isValid = [emailTest evaluateWithObject:self];
   return isValid;
