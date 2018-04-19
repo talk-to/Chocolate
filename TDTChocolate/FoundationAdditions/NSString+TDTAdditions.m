99d23f31b1d903644e15873049d8f16b2881059f
Nitesh Vijay <niteshvijay1995@gmail.com>
2018-04-19 16:08:28 +0530

    Fix for loop syntax

--- TDTChocolate/FoundationAdditions/NSString+TDTAdditions.m
+++ TDTChocolate/FoundationAdditions/NSString+TDTAdditions.m
@@ -125,8 +125,9 @@
   CC_MD5(ptr, strlen(ptr), md5Buffer);
 
   NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
-  for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+  for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
     [output appendFormat:@"%02x",md5Buffer[i]];
+  }
   
   return output;
 }
